5b3f17354d1e55f6bfae5fa978731ce7bee5eaca
juliensallei <magna@live.fr>
2016-03-22 11:37:44 +0100

    Displaying sessions and errors

--- index.php
+++ index.php
@@ -5,5 +5,8 @@ $page = showPage();
 include('includes/header.php');
 include ('pages/'.$page['content']);
 include ('includes/footer.php');
+print_r($_SESSION);
+error_reporting(E_ALL);
+ini_set("display_errors",1);
 ?>
 
