030e94957fbe051a69d8f03c50eec4ac94460c0b
juliensallei <magna@live.fr>
2016-03-22 12:02:21 +0100

    Testing drag and drop in inner page.

--- index.php
+++ index.php
@@ -5,6 +5,9 @@ $page = showPage();
 include('includes/header.php');
 include ('pages/'.$page['content']);
 include ('includes/footer.php');
+?>
+<script src="javascript/script.js"></script>
+<?php
 print_r($_SESSION);
 error_reporting(E_ALL);
 ini_set("display_errors",1);
