b19fbfd849cf542bfd7fe44cb3dbb230859489d9
OphelieF <ophefaivre@gmail.com>
2016-03-24 16:10:46 +0100

    Add content on page liste-commande v1

---
+++ pages/liste-commande.php
@@ -0,0 +1,12 @@
+<h1>Liste des commandes</h1>
+<h2>Filtres : </h2>
+<a href="#"><img scr="" alt="noir"><p>Non traités</p></a>
+<a href="#"><img scr="" alt="vert"><p>Acceptées</p></a>
+<a href="#"><img scr="" alt="orange"><p>En attente</p></a>
+<a href="#"><img scr="" alt="rouge"><p>Annulées</p></a>
+
+<div class="commande">
+	<p class="info-commande">N° Commande : </p><p id="numero-commande">123456</p><img src="" />
+	<p class="info-commande">Nom : </p><p id="nom-commande">Pascal Poirey</p>
+	<p class="info-commande">Date : </p><p id="nom-commande">Pascal Poirey</p>
+</div>
