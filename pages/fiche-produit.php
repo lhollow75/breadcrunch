4b381396a339db6a1a9775045f1dfef8e4747e99
OphelieF <ophefaivre@gmail.com>
2016-03-24 12:39:29 +0100

    Create fiche-produit.php

---
+++ pages/fiche-produit.php
@@ -0,0 +1,11 @@
+<h1>Pain au chocolat</h1>
+<img src="images/breadcrunch_29.jpg" alt="">
+
+<h4>Ingrédients</h4>
+<p>Sucre, oeufs, farine, et d'autres trucs</p>
+<p>Délai de commande : </p><p>2h</p>	
+<label for="pickup-date">Date de retrait</label><input type="date" name="pickup-date" id="pickup-date">
+<p>Heure de retrait</p>
+<p>Quantité</p>
+<p>Allergène</p>
+<p>Taille gateaux</p>
