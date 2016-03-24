dd49d4919336fa4dafc13dc83bc12a3ad4394dfb
Matilde <Matilde>
2016-03-24 16:22:53 +0100

    modification of the modifEnBase fonction

--- functionBdd.php
+++ functionBdd.php
@@ -3,7 +3,7 @@ require_once ('php/config.php');
 if (isset($_POST['fonction'])){
 	switch ($_POST['fonction']) {
 		case 'localisationEnBase':
-			localisationEnBase($mysql, $_POST['data'], $_POST['donnees']);
+			localisationEnBase($mysql, $_POST['data'], $_POST['donnees'], $_POST['action']);
 			break;
 		
 		default:
@@ -13,8 +13,20 @@ if (isset($_POST['fonction'])){
 }
 
 
-function recupProduit(){
-
+function recupEnBase($mysql, $table, $colonne, $row){
+	$req = $mysql->prepare("SELECT $colonne
+							FROM $table
+							WHERE id = :id");
+	$req->execute(array(
+	':id'=> $row
+	));
+	if($req->rowCount()>=1) {
+		$reponse = $req->fetch();
+		//var_dump($reponse[$colonne]);
+		echo utf8_encode($reponse[$colonne]);
+	} else {
+		return "Erreur lors de la récupération";
+	}
 }
 
 function recupTable($mysql, $table){
@@ -46,14 +58,19 @@ function recupAccueil($mysql){
 	}
 }
 
-function localisationEnBase($mysql, $id, $donnees){
+function localisationEnBase($mysql, $id, $donnees, $action){
 	$req = $mysql->prepare("SELECT * FROM mapping WHERE id_div= :id");
 	$req->execute(array(
 		':id'=>$id
 		));
 
 	$reponse = $req->fetch();
-	modifEnBase($mysql, $donnees, $reponse['table_insert'], $reponse['colonne_insert'], $reponse['id_insert']);
+	if ($action == 'modification'){
+		modifEnBase($mysql, $donnees, $reponse['table_insert'], $reponse['colonne_insert'], $reponse['id_insert']);
+	}else if ($action == 'recuperation'){
+		recupEnBase($mysql, $reponse['table_insert'], $reponse['colonne_insert'], $reponse['id_insert']);
+	}
+	
 }
 
 function modifEnBase($mysql, $insertion, $table, $colonne, $row){
@@ -61,7 +78,7 @@ function modifEnBase($mysql, $insertion, $table, $colonne, $row){
 							SET $colonne = :insertion
 							WHERE id = :id");
 	$req->execute(array(
-	':insertion'=>utf8_decode(htmlspecialchars($insertion)),
+	':insertion'=>utf8_decode(strip_tags($insertion)),
 	':id'=> $row
 	));
 }
