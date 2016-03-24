90e8bae2ba3d57728f940446adb05743fc1cd889
juliensallei <magna@live.fr>
2016-03-24 11:59:31 +0100

    Integrating upload files to the new html.

--- pages/index.php
+++ pages/index.php
@@ -1,10 +1,20 @@
-	<img src="images/breadcrunch_02.jpg" alt="">
+    <?php 
+    include('./functionBdd.php');
+    $accueil = recupAccueil($mysql);
+    $categorie = recupTable($mysql,'categorie');
+    ?>
+    <img src="images/breadcrunch_02.jpg" alt="">
 	<div>
 		<h1>Lorem ipsum dolor sit amet</h1>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui blanditiis distinctio sunt, laborum incidunt itaque?</p>
 	</div>
 	<div id="galery">
-		<img class="photo-galery" src="images/breadcrunch_05.jpg" alt="">
+		<img id="img_baguette" class="photo-galery" src="./img/<?php echo $categorie[0]['photo']?>" alt="">
+        <form style="display:<?php echo $appear; ?>;" method="post" enctype="multipart/form-data" action="./img_upload.php">
+            <input  class="fileimg" type="file" name="fichier"  accept="image/*" multiple>
+            <input type="hidden" name="id_formulaire" value="envoyer_baguette">
+            <button type="submit" >Envoyer</button>
+        </form>
 		<img class="photo-galery" src="images/breadcrunch_07.jpg" alt="">
 		<img class="photo-galery" src="images/breadcrunch_09.jpg" alt="">
 		<img class="photo-galery" src="images/breadcrunch_11.jpg" alt="">
