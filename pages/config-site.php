827211900aa7be55ed2f7827c3a5345806b316f1
OphelieF <ophefaivre@gmail.com>
2016-03-24 17:59:38 +0100

    Content on page config-site v1

--- pages/config-site.php
+++ pages/config-site.php
@@ -1,8 +1,46 @@
 <h1>Configuration du site</h1>
+ 
 
 <div>
-	Catgéories proposées : 
+	Catégories proposées : 
 	<input type="checkbox" name="box-boulangerie" id="box-boulangerie">
 	<label for="box-boulangerie">Boulangerie</label>
+	<input type="checkbox" name="box-patisserie" id="box-patisserie">
+	<label for="box-patisserie">Patisserie</label>
+	<input type="checkbox" name="box-chocolaterie" id="box-chocolaterie">
+	<label for="box-chocolaterie">Chocolaterie</label>
+	<input type="checkbox" name="box-sandwich-salade" id="box-sandwich-salade">
+	<label for="box-Ssandwich-salade">Sandwich/Salade</labelSandwich/Salade</div>
+</div>
 
+<div>
+	Je souhaite activer le module de commande en ligne avec retrait en magasin : 
+	<input type="checkbox" name="commande-active" id="commande-active">
+	<label for="commande-active">Oui</label>
+	<input type="checkbox" name="commande-desactive" id="commande-desactive">
+	<label for="commande-desactive">Non</label>
+</div>
+
+<div>
+	Je souhaite activer le module de paiement en ligne : 
+	<input type="checkbox" name="paiement-active" id="paiement-active">
+	<label for="paiement-active">Oui</label>
+	<input type="checkbox" name="paiement-desactive" id="paiement-desactive">
+	<label for="paiement-desactive">Non</label>
+</div>
+
+div>
+	Je souhaite faire apparaitre les réseaux sociaux suivants : 
+	<label for="facebook-active">Lien Facebook</label>
+	<input type="text" name="facebook-active" id="facebook-active">
+	<label for="twitter-active">Lien Twitter</label>
+	<input type="text" name="twitter-active" id="twitter-active">
+	<label for="instagram-active">Lien Instagram</label>
+	<input type="text" name="instagram-active" id="instagram-active">
+	<label for="pinterest-active">Lien Pinterest</label>
+	<input type="text" name="pinterest-active" id="pinterest-active">
+	<label for="youtube-active">Lien Youtubeyoutube</label>
+	<input type="text" name="youtube-active" id="youtube-active">
+
+	
 </div>
