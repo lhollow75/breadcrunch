ea33794d1961f731cb7adda39c710930cc251244
juliensallei <magna@live.fr>
2016-03-24 16:10:35 +0100

    Setting up, HTML product page

--- pages/fiche-produit.php
+++ pages/fiche-produit.php
@@ -1,10 +1,13 @@
-<h1>Pain au chocolat</h1>
-<img class="product-picture" src="images/breadcrunch_29.jpg" alt="">
+<div class="wrapper" id="main-content">
+<div class="container">
+<h1 class="product-name">Pain au chocolat</h1>
+<img class="col-lg-7 product-picture" src="images/breadcrunch_29.jpg" alt="">
 
+<div class="product-specs col-lg-4">
 <h4>Ingrédients</h4>
 <p>Sucre, oeufs, farine, et d'autres trucs</p>
-<label>Délai de commande : </p><p>2h</p>
-<label for="pickup-date">Date de retrait </label><input type="date" name="pickup-date" id="pickup-date"></br>
+<label>Délai de commande minimum : </label><p>2h</p>
+<label for="pickup-date">Date de retrait </label><input type="date" name="pickup-date" id="pickup-date"></br></br>
 <label>Heure de retrait </label>
 	<select>
 	  <option value="9h-10h">9h - 10h</option>
@@ -16,8 +19,9 @@
 	  <option value="15h-16h">15h - 16h</option>
 	  <option value="16h-17h">16h - 17h</option>
 	  <option value="17h-18h">17h - 18h</option>
-	</select></br>
+	</select></br></br>
 
-<label for="quantity">Quantité </label><input type="number" name="quantity" id="quantity">
-<label>Allergène : </p><p>Poissons et produits à base de crustacé</p>
-<label>Taille gateaux</p>
+<label for="quantity">Quantité</label><input type="number" name="quantity" id="quantity"></br></br>
+<label>Allergène : </label><p>Poissons et produits à base de crustacé</p>
+<label>Taille gateaux</label>
+</div></div></div>
