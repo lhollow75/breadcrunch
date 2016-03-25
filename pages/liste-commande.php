1466fbc20ec25d6721199246b5acaf131b44a490
juliensallei <magna@live.fr>
2016-03-25 11:43:55 +0100

    CSS for product-page and orders page.

--- pages/liste-commande.php
+++ pages/liste-commande.php
@@ -1,41 +1,125 @@
 <h1>Liste des commandes</h1>
-<h2>Filtres : </h2>
-<a href="#"><img scr="" alt="noir"><p>Non traités</p></a>
-<a href="#"><img scr="" alt="vert"><p>Acceptées</p></a>
-<a href="#"><img scr="" alt="orange"><p>En attente</p></a>
-<a href="#"><img scr="" alt="rouge"><p>Annulées</p></a>
+<div class="wrapper" id="main-content">
+<div class="container">
+<div></div>
+<ul class="filter-list">
+    <li>
+        <h2>Filtres : </h2>
+    </li>   
+    <li>
+        <a href="#"><p>Non traités</p></a>
+    </li>
+    <li>
+    <img scr="" alt="noir">
+    </li>
+    <li>
+        <a href="#"><p>Acceptées</p></a>
+    </li>
+    <li>
+        <img scr="" alt="vert">
+    </li>
+    <li>
+        <a href="#"><p>En attente</p></a>
+    </li>
+    <li>
+        <img scr="" alt="orange">
+    </li>
+    <li>
+        <a href="#"><p>Annulées</p></a>
+    </li>
+    <li>
+        <img scr="" alt="rouge">
+    </li>
+</ul>
 
 <div class="commande row">
-	<p class="info-commande">N° Commande : </p><p id="numero-commande">123456</p><img src="" />
-	<p class="info-commande">Nom : </p><p id="nom-client">Pascal Poirey</p>
-	<p class="info-commande">Date : </p><p id="date-commande">28/03/2016</p>
-	<p class="info-commande">Heure : </p><p id="date-commande">16h-17h</p>
-	<div class="product col-lg-6 col-md-6 col-sm-12 col-xs-12">
-		<img src="" alt="">
-		<p id="name-product">Pain au chocolat</p>
-		<p class="info">Pour offrir : </p><img src=""/>
-		<p class="info">Message </p><p id="message">Joyeux anniversaire Machin</p>
-		<p id="price">1,50€</p>
-	</div>
-	<div class="product col-lg-6 col-md-6 col-sm-12 col-xs-12">
-		<img src="" alt="">
-		<p id="name-product">Pain au chocolat</p>
-		<p class="info">Pour offrir : </p><img src=""/>
-		<p class="info">Message </p><p id="message">Joyeux anniversaire Machin</p>
-		<p id="price">1,50€</p>
-	</div>
-	<div class="product col-lg-6 col-md-6 col-sm-12 col-xs-12">
-		<img src="" alt="">
-		<p id="name-product">Pain au chocolat</p>
-		<p class="info">Pour offrir : </p><img src=""/>
-		<p class="info">Message </p><p id="message">Joyeux anniversaire Machin</p>
-		<p id="price">1,50€</p>
-	</div>
-
-	<p>Commentaire (demande particulière,...) :</p>
-	<textarea></textarea>
-	<p>Total : </p><p id="total-price">13,50€</p>
-	<button class="btn">Accepter la commande</button>
-	<button class="btn">Mettre en attente</button>
-	<button class="btn">Annuler la commande</button>
+<div class="data-product">
+    <ul>
+        <li>
+	    <p class="info-commande">N° Commande : </p>
+	    </li>
+	    <li>
+	    <p id="numero-commande">123456</p>
+	    </li>
+	    <li>
+	    <img src="" />
+	    </li>
+	</ul>
 </div>
+<div class="rowdataproduct row">
+    <ul class="dataproduct-list">
+        <li>
+	    <p class="info-commande">Nom : </p>
+	    </li>
+	    <li>
+	    <p id="nom-client">Pascal Poirey</p>
+	    </li>
+	    <li>
+	    <p class="info-commande">Date : </p>
+	    </li>
+	    <li>
+	    <p id="date-commande">28/03/2016</p>
+	    </li>
+	    <li>
+	    <p class="info-commande">Heure : </p>
+	    </li>
+	    <li>
+	    <p id="date-commande">16h-17h</p>
+	    </li>
+	</ul>
+</div>
+<div class="product-contents">
+	<div class="product col-sm-offset-0 col-xs-offset-0 col-lg-5 col-md-5 col-sm-10 col-xs-10">
+	    <div class="product-space">
+		<img src="" alt="">
+		<p id="name-product">Pain au chocolat</p>
+		<p class="info">Pour offrir : </p><img src=""/>
+		<p class="info">Message </p><p id="message">Joyeux anniversaire Machin</p>
+		<p id="price">1,50€</p>
+        </div>
+	</div>
+	
+	<div class="product col-md-offset-1 col-sm-offset-0 col-xs-offset-0 col-lg-offset-1 col-lg-5 col-md-5 col-sm-10 col-xs-10">
+	    <div class="product-space">
+		<img src="" alt="">
+		<p id="name-product">Pain au chocolat</p>
+		<p class="info">Pour offrir : </p><img src=""/>
+		<p class="info">Message </p><p id="message">Joyeux anniversaire Machin</p>
+		<p id="price">1,50€</p>
+        </div>
+    </div>
+
+	<div class="product  col-sm-offset-0 col-xs-offset-0 col-lg-5 col-md-5 col-sm-10 col-xs-10">
+	    <div class="product-space">
+		<img src="" alt="">
+		<p id="name-product">Pain au chocolat</p>
+		<p class="info">Pour offrir : </p><img src=""/>
+		<p class="info">Message </p><p id="message">Joyeux anniversaire Machin</p>
+		<p id="price">1,50€</p>
+        </div>
+	</div>
+    
+    <div class="row productend col-sm-10 col-xs-10">
+        <div class="col-lg-8">
+            <p></br>Commentaire (demande particulière,...) :</p>
+	        <textarea></textarea>
+        </div>
+	        <div class="col-lg-4">
+	        <ul>
+	        <li>
+	        <p>Total : </p>
+            </li>
+            </br><li>
+                <a id="total-price">13,50€</a>
+            </li>
+            </ul>
+            </div>
+	    </div>
+    </div>
+    <div class="col-lg-12 buttons">
+	    <button class="btn">Accepter la commande</button>
+	    <button class="btn">Mettre en attente</button>
+	    <button class="btn">Annuler la commande</button>
+    </div>
+</div>
+    </div></div>
