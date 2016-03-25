7c66c3e18145519e86639a15a86ae5941dd99d27
mrvink <vinkdesign@gmail.com>
2016-03-25 14:54:10 +0100

    update page story - id and class

--- pages/a-propos.php
+++ pages/a-propos.php
@@ -5,20 +5,20 @@
     <div class="wrapper" id="main-content">
         <div class="container">
 			<div id="story">
-				<h1 id="story_titre" contenteditable="<?php echo $activeContent; ?>"><?php localisationEnBase($mysql, 'titre_index','', 'recuperation') ?></h1>
+				<h1 id="story_titre">Notre histoire</h1>
 		        <hr>
 			</div><!-- /#story -->
 
 		<div class="col-lg-6 col-xs-12">
-			<div class="story-img">
+			<div class="story-section">
 				<img src="./img/story_boulanger.jpg" alt="">
 			</div>
 		</div>
 
 		<div class="col-lg-6 col-xs-12">
-			<div class="story-text">
-				<h2>Imaginé et créé par Michel Chesnaux</h2>
-				<p>Naît à Saulges un 28 décembre. Ses parents sont agriculteurs et, très tôt, il reçoit une éducation rigoureuse qui lui donne le goût du travail et des valeurs simples qui ne le quitteront jamais. Sa vocation s’impose à lui : il sera boulanger-pâtissier.
+			<div class="story-section">
+				<h2 id="story-section-title">Imaginé et créé par Michel Chesnaux</h2>
+				<p id="story-section-text">Naît à Saulges un 28 décembre. Ses parents sont agriculteurs et, très tôt, il reçoit une éducation rigoureuse qui lui donne le goût du travail et des valeurs simples qui ne le quitteront jamais. Sa vocation s’impose à lui : il sera boulanger-pâtissier.
 A 22 ans, il monte sa propre affaire à Bellême, dans l’Orne. Mais bientôt, il rêve de pâtisserie nouvelle et de recettes innovantes : après avoir suivi les stages de l’Ecole Lenôtre, Michel ouvre Le Trianon, à Angers, qui devient vite le must de la ville.
 L’étape suivante sera la consécration de son savoir-faire avec l’admission dans la prestigieuse association Relais Dessert International qui consacre, de par le monde, les cent meilleurs artisans de la gastronomie : en 1988, Michel en fait non seulement partie, mais il en remporte le trophée.</p>
 
