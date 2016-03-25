4d4ae797933abc6f499477f6010442d661ccbae2
mrvink <vinkdesign@gmail.com>
2016-03-25 11:21:24 +0100

    Update footer.php and fonts folder

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,46 @@
-    <footer>
-        <div><p>Lorem ipsum dolor sit</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, eius.</p></div>
-        
-        <div id="social-icone"></div>
+	<footer>
+	    <div class="wrapper" id="main-content">
+    	    <div class="container">
+        		<div class="row">
+
+        			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+	        				<div id="footer-presentation">
+	        				<h3>Qui sommes-nous ?</h3>
+	        				<p>Fusce mauris magna, ornare aliquam velit ac, ullamcorper semper ligula. Nulla commodo sed nibh at hendrerit. Ut vel ornare metus, non imperdiet tortor.</p>
+	        			</div>
+					</div>
+
+        			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+	                	<div id="footer-social">
+	                		<h3>Suivez-nous !</h3>
+					        <div id="social-icons">
+					        	<ul>
+					        		<li><a href="#"><span class="icon-facebook"></span></a></li>
+					        		<li><a href="#"><span class="icon-twitter"></span></a></li>
+					        		<li><a href="#"><span class="icon-instagram"></span></a></li>
+					        		<li><a href="#"><span class="icon-pinterest"></span></a></li>
+					        		<li><a href="#"><span class="icon-youtube"></span></a></li>
+					        	</ul>
+					        </div>
+					    </div>
+				    </div>
+
+        			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+				    	<div id="footer-location">
+	                		<h3>Où nous trouver ?</h3>
+	                		<p class="location-street">2, rue de la Monnaie</p>
+							<p>
+								<span class="location-cp">10000</span>
+								<span class="location-city">Troyes</span>
+							</p>
+							<p class="location-phone-number">+33 (0)3 25 80 38 38</p>
+							<p class="location-mail">infos@pascal-caffet.com</p>
+					    </div>
+				    </div>
+
+
+
+
     </footer>
 
     <!-- message erreur or success -->
