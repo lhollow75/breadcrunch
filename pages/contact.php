dc56fceafeb815b55aa9f9ab2a7ef01054852be3
Matilde <Matilde>
2016-03-25 15:36:30 +0100

    add contenteditable in the contact page

--- pages/contact.php
+++ pages/contact.php
@@ -9,7 +9,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-12">
-				<h1>CONTACT</h1>
+				<h1 contenteditable="<?php echo $activeContent; ?>">CONTACT</h1>
 			</div>
 			<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
 				<form action="" method="post" id="contact-form" class="row">
@@ -26,16 +26,16 @@
 			</div>
 			<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
 				<div class="contact-details">
-					<p class="col-lg-12">Le Lorem Ipsum est simplement du faux texte employé dans la compo- sition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de </p>
+					<p class="col-lg-12" contenteditable="<?php echo $activeContent; ?>">Le Lorem Ipsum est simplement du faux texte employé dans la compo- sition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de </p>
 					<p class="col-lg-5 ">
 						<span class="details">Adresse</span>
 						<span class="details">Téléphone</span>
 						<span class="details">Email</span>
 					</p>
 					<p class="col-lg-7">
-						<span class="dtl">Siège Social<br>2, rue de la Monnaie<br>10000 Troyes</span>
-						<span class="dtl">+33 (0)3 25 80 38 38</span>
-						<span class="dtl">infos@pascal-caffet.com</span>
+						<span class="dtl" contenteditable="<?php echo $activeContent; ?>">Siège Social<br>2, rue de la Monnaie<br>10000 Troyes</span>
+						<span class="dtl" contenteditable="<?php echo $activeContent; ?>">+33 (0)3 25 80 38 38</span>
+						<span class="dtl" contenteditable="<?php echo $activeContent; ?>">infos@pascal-caffet.com</span>
 					</p>
 				</div>
 				<div class="social-network">
@@ -48,7 +48,7 @@
 			</div>
 		</div>
 	</div>
-	<script src='https://maps.googleapis.com/maps/api/js?v=3.exp'></script>
+	<!-- <script src='https://maps.googleapis.com/maps/api/js?v=3.exp'></script>
 	<div style='overflow:hidden;height:400px;width:100%;'>
 	<div id='gmap_canvas' style='height:400px;width:100%;'></div>
 		<div>
@@ -62,8 +62,10 @@
 			</small>
 		</div>
 		<style>#gmap_canvas img{max-width:none!important;background:none!important}</style>
-	</div>
-	<script type='text/javascript'>function init_map(){var myOptions = {zoom:15,center:new google.maps.LatLng(48.86031277478549,2.3650750455994007),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(48.86031277478549,2.3650750455994007)});infowindow = new google.maps.InfoWindow({content:'<strong>Boulangerie </strong><br>Paris, France<br>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script>
+	</div> -->
+	<img src="https://maps.googleapis.com/maps/api/staticmap?size=700x200&zoom=15&maptype=roadmap\
+&markers=size:mid%7Ccolor:red%7C6+rue=froment,Paris&key=AIzaSyDEIF4I4xIgInAxD6qxo52UzbTvi6KlO1k">
+	<!-- <script type='text/javascript'>function init_map(){var myOptions = {zoom:15,center:new google.maps.LatLng(48.86031277478549,2.3650750455994007),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(48.86031277478549,2.3650750455994007)});infowindow = new google.maps.InfoWindow({content:'<strong>Boulangerie </strong><br>Paris, France<br>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script> -->
 		
 </div>
 
