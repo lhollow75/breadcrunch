1e5c032bbe70c9b9bcc927865da576ce1aa03287
Shanez <shanez@MacBook-Pro-de-Shanez.local>
2016-03-29 22:16:40 +0200

    correction panier

--- pages/panier.php
+++ pages/panier.php
@@ -13,51 +13,57 @@
 				<hr>
 			</div>
 			<form action="" method="post" id="chart-form" class="row">
-				<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-					<img src="./images/pains-au-chocolat.png" alt="">
-				</div>
-				<div class="chart-infos col-lg-9 col-md-9 col-sm-12 col-xs-12">
-					<p class="row">
-						<span class="col-lg-4 col-md-4 col-sm-10 col-xs-10">Pain au chocolat</span><span class="col-lg-2 col-md-2 col-sm-2 col-xs-2">x1</span><span class="col-lg-6 col-md-6 hidden-sm hidden-xs">....................................................1,50€</span>
-					</p>
-					<div class="row">
-						<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 gift">
-							<label for="gift">Pour offrir<input id="gift" type="checkbox"></label>
-						</div>
-						<div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
-							<input type="text" name="message" value="" placeholder="Message..">
+				<div class="row" id="row-panier">
+					<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+						<img src="./images/pains-au-chocolat.png" alt="">
+					</div>
+					<div class="chart-infos col-lg-9 col-md-9 col-sm-12 col-xs-12">
+						<p class="row">
+							<span class="col-lg-4 col-md-4 col-sm-10 col-xs-10">Pain au chocolat</span><span class="col-lg-2 col-md-2 col-sm-2 col-xs-2">x1</span><span class="col-lg-6 col-md-6 hidden-sm hidden-xs">....................................................1,50€</span>
+						</p>
+						<div class="row">
+							<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 gift">
+								<label for="gift">Pour offrir<input id="gift" type="checkbox"></label>
+							</div>
+							<div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+								<input type="text" name="message" value="" placeholder="Message..">
+							</div>
 						</div>
 					</div>
 				</div>
-				<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-					<img src="./images/macarons-feature-pic.png" alt="">
-				</div>
-				<div class="chart-infos col-lg-9 col-md-9 col-sm-12 col-xs-12">
-					<p class="row">
-						<span class="col-lg-4 col-md-4 col-sm-10 col-xs-10">Macarons</span><span class="col-lg-2 col-md-2 col-sm-2 col-xs-2">x1</span><span class="col-lg-6 col-md-6 hidden-sm hidden-xs">....................................................5,00€</span>
-					</p>
-					<div class="row">
-						<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 gift">
-							<label for="gift">Pour offrir<input id="gift" type="checkbox"></label>
-						</div>
-						<div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
-							<input type="text" name="message" value="" placeholder="Message..">
+				<div class="row" id="row-panier">
+					<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+						<img src="./images/macarons-feature-pic.png" alt="">
+					</div>
+					<div class="chart-infos col-lg-9 col-md-9 col-sm-12 col-xs-12">
+						<p class="row">
+							<span class="col-lg-4 col-md-4 col-sm-10 col-xs-10">Macarons</span><span class="col-lg-2 col-md-2 col-sm-2 col-xs-2">x1</span><span class="col-lg-6 col-md-6 hidden-sm hidden-xs">....................................................5,00€</span>
+						</p>
+						<div class="row">
+							<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 gift">
+								<label for="gift">Pour offrir<input id="gift" type="checkbox"></label>
+							</div>
+							<div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+								<input type="text" name="message" value="" placeholder="Message..">
+							</div>
 						</div>
 					</div>
 				</div>
-				<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-					<img src="./images/tarte-au-chocolat-de-frederic-anton.png" alt="">
-				</div>
-				<div class="chart-infos col-lg-9 col-md-9 col-sm-12 col-xs-12">
-					<p class="row">
-						<span class="col-lg-4 col-md-4 col-sm-10 col-xs-10">Tarte au chocolat</span><span class="col-lg-2 col-md-2 col-sm-2 col-xs-2">x1</span><span class="col-lg-6 col-md-6 hidden-sm hidden-xs">....................................................7,00€</span>
-					</p>
-					<div class="row">
-						<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 gift">
-							<label for="gift">Pour offrir<input id="gift" type="checkbox"></label>
-						</div>
-						<div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
-							<input type="text" name="message" value="" placeholder="Message..">
+				<div class="row" id="row-panier">
+					<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+						<img src="./images/tarte-au-chocolat-de-frederic-anton.png" alt="">
+					</div>
+					<div class="chart-infos col-lg-9 col-md-9 col-sm-12 col-xs-12">
+						<p class="row">
+							<span class="col-lg-4 col-md-4 col-sm-10 col-xs-10">Tarte au chocolat</span><span class="col-lg-2 col-md-2 col-sm-2 col-xs-2">x1</span><span class="col-lg-6 col-md-6 hidden-sm hidden-xs">....................................................7,00€</span>
+						</p>
+						<div class="row">
+							<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 gift">
+								<label for="gift">Pour offrir<input id="gift" type="checkbox"></label>
+							</div>
+							<div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+								<input type="text" name="message" value="" placeholder="Message..">
+							</div>
 						</div>
 					</div>
 				</div>
