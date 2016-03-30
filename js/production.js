d9ef0a2b816681282c1116d0a6013b4926938614
Shanez <shanez@MBP-de-Shanez.wifi.froment.in.iesanetwork.com>
2016-03-30 15:51:25 +0200

    modif menu scroll to

--- js/production.js
+++ js/production.js
@@ -346,6 +346,19 @@ function showHide(valeur, box){
 
 
 
+//ScrollTo
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script>
+	$(document).ready(function() {
+		$('.js-scrollTo').on('click', function() { // Au clic sur un élément
+			var page = $(this).attr('href'); // Page cible
+			var speed = 750; // Durée de l'animation (en ms)
+			$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+			return false;
+		});
+	});
+</script>
+
 
 
 //on cible tous les éléments required
