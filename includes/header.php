e1a7c32cd5123fd1138c64afd262704907e0aaf0
Shanez <shanez@MBP-de-Shanez.wifi.froment.in.iesanetwork.com>
2016-03-30 15:22:26 +0200

    modif menu

--- includes/header.php
+++ includes/header.php
@@ -99,11 +99,11 @@
         <!-- second-menu -->  
         <nav id="second-menu" class="menu-burger">
             <ul>
-                <li><a id="menu-boulangerie" href="#">Boulangerie</a></li>
-                <li><a id="menu-patisserie" href="#">Pâtisserie</a></li>
-                <li><a id="menu-sandwich" href="#">Sandwich / Salade</a></li>
-                <li><a id="menu-chocolaterie" href="#">Chocolaterie</a></li>
-                <li><a id="menu-boisson" href="#">Boisson</a></li>
+                <li><a id="menu-boulangerie" href="index.php?page=categorie#description-Boulangerie">Boulangerie</a></li>
+                <li><a id="menu-patisserie" href="index.php?page=categorie#description-Patisserie">Pâtisserie</a></li>
+                <li><a id="menu-sandwich" href="index.php?page=categorie#description-Sandwich">Sandwich / Salade</a></li>
+                <li><a id="menu-chocolaterie" href="index.php?page=categorie#description-Chocolaterie">Chocolaterie</a></li>
+                <li><a id="menu-boisson" href="index.php?page=categorie#description-Boisson">Boisson</a></li>
             </ul>
         </nav>
         <!-- second-menu -->  
